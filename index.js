d29d20145650466a9ed7704447c1c04b46a1a855
Ryan Murphy <rdmurphy@users.noreply.github.com>
2018-03-19 22:37:43 -0500

    Allow for filtering by origin

--- index.js
+++ index.js
@@ -8,17 +8,39 @@ const Raven = require('raven');
 // local
 const { processLookupMatch } = require('./utils');
 
+// if the SENTRY_DSN environment variable is provided, hook up Sentry
 if (process.env.SENTRY_DSN) {
   Raven.config(process.env.SENTRY_DSN).install();
 }
 
+// we only allow approved origins in, so this environment variable is required
+const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN;
+if (!ALLOWED_ORIGIN)
+  throw new Error('Please provide an ALLOWED_ORIGIN environmental variable');
+
+// ready to test origins
+const origin_re = new RegExp(ALLOWED_ORIGIN);
+
+// setting up the lookup is slow, so do it first synchronously
 const lookup = maxmind.openSync('./GeoLite2-City.mmdb');
 
 module.exports = (req, res) => {
   // a hook to let deployment platforms know we're ready to go
   if (req.url === '/health') {
     res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     return 'OK';
+  }
+
+  // now let's make sure the origin we're working with is valid before going
+  // forward
+  const origin = req.headers['origin'];
+
+  // if there is no origin provided, or it fails the regex test, abort
+  if (!origin || !origin_re.test(origin)) {
+    res.statusCode = 401;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    return 'Unauthorized';
   }
 
   // cache nothing, please: https://stackoverflow.com/a/2068407
@@ -28,7 +50,7 @@ module.exports = (req, res) => {
   res.setHeader('Vary', '*');
 
   // TODO: allow filtering of origins
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', origin);
 
   // the requesting service's IP
   let ip = req.connection.remoteAddress;
@@ -40,7 +62,7 @@ module.exports = (req, res) => {
   if (urlParts.query.ip) {
     ip = urlParts.query.ip;
 
-    // if an IP was passed along in the X-Forwarded-For header, use that
+    // or if an IP was passed along in the X-Forwarded-For header, use that
   } else if (req.headers['x-forwarded-for']) {
     ip = req.headers['x-forwarded-for'].split(/,\s+/)[0];
   }
